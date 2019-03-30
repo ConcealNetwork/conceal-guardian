7497b8d5d2c3b33ec50cc885b1430528514a58b3
bomb-on <0nada0@gmail.com>
2019-03-30 15:12:46 +0000

    Added import and fixed path in utils, updated .gitignore

--- utils.js
+++ utils.js
@@ -7,32 +7,32 @@
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
+const UUID = require("pure-uuid");
 
 module.exports = {
-    ensureUserDataDir: function() {
-        var userDataDir = process.env.APPDATA || (
-          process.platform == "darwin"
-          ? process.env.HOME + "Library/Preferences"
-          : process.env.HOME + "/.local/share");
-        userDataDir = path.join(userDataDir, "ConcealNodeGuard");
-    
-        if (!fs.existsSync(userDataDir)) {
-          fs.mkdirSync(userDataDir);
-        }
-    
-        return userDataDir;
-      },
-    
-      ensureNodeUniqueId: function() {
-        var nodeDataFile = path.join(this.ensureUserDataDir(), "nodedata.json");
-    
-        if (fs.existsSync(nodeDataFile)) {
-          var nodeData = JSON.parse(fs.readFileSync(nodeDataFile));
-          return nodeData.id;
-        } else {
-          var nodeData = { id: new UUID(4).format() }
-          fs.writeFileSync( nodeDataFile, JSON.stringify(nodeData), "utf8");
-          return nodeData.id;
-        }
-      }
-    };
+  ensureUserDataDir: function() {
+    var userDataDir = process.env.APPDATA || (
+      process.platform === "darwin"
+      ? process.env.HOME + "/Library/Preferences"
+      : process.env.HOME + "/.local/share");
+    userDataDir = path.join(userDataDir, "ConcealNodeGuard");
+
+    if (!fs.existsSync(userDataDir)) {
+      fs.mkdirSync(userDataDir);
+    }
+
+    return userDataDir;
+  },
+  ensureNodeUniqueId: function() {
+    var nodeDataFile = path.join(this.ensureUserDataDir(), "nodedata.json");
+
+    if (fs.existsSync(nodeDataFile)) {
+      var nodeData = JSON.parse(fs.readFileSync(nodeDataFile));
+      return nodeData.id;
+    } else {
+      var nodeData = { id: new UUID(4).format() }
+      fs.writeFileSync( nodeDataFile, JSON.stringify(nodeData), "utf8");
+      return nodeData.id;
+    }
+  }
+};
