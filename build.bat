641cbef31b7771d078a9d15021566962faa87a39
Taegus <taegus.cromis@gmail.com>
2019-04-02 13:37:43 +0200

    bin to ignore list changed executable names to reflect the platform

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-call pkg index.js --targets node10-win-x64 --output .\bin\win\guardian.exe
-call pkg index.js --targets node10-linux-x64 --output .\bin\linux\guardian
-call pkg index.js --targets node10-macos-x64 --output .\bin\macos\guardian
+call pkg index.js --targets node10-win-x64 --output .\bin\win\guardian-win64.exe
+call pkg index.js --targets node10-linux-x64 --output .\bin\linux\guardian-linux64
+call pkg index.js --targets node10-macos-x64 --output .\bin\macos\guardian-macos64
 
 rem call .\tools\upx.exe .\bin\win\guardian.exe
 rem call .\tools\upx.exe .\bin\linux\guardian
