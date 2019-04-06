2513837b1ee95a4c784f1f529835d3552cec3044
Taegus <taegus.cromis@gmail.com>
2019-04-06 22:37:41 +0200

    use shelljs on all platforms missing packages added to package.json

--- service.js
+++ service.js
@@ -2,29 +2,12 @@
 //
 // Please see the included LICENSE file for more information.
 
-const childProcess = require("child_process");
 const xmlbuilder = require("xmlbuilder");
 const format = require("string-template");
 const shell = require("shelljs");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
-
-function os_func() {
-  this.execCommand = function (cmd, callback) {
-    childProcess.exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error("\nexec error: " + error);
-        return;
-      }
-
-      callback(stdout);
-    });
-  };
-}
-
-// create new os opject
-const procTools = new os_func();
 
 // export functions
 module.exports = {
@@ -41,9 +24,7 @@ module.exports = {
         if (err) {
           console.log('\nError trying to save the XML: ' + err);
         } else {
-          procTools.execCommand('cgservice.exe install', function (returnvalue) {
-            console.log("\n" + returnvalue);
-          });
+          shell.exec('cgservice.exe install');
         }
       });
     } else if (process.platform == "linux") {
@@ -59,59 +40,53 @@ module.exports = {
         if (err) {
           console.log('\nError trying to save the service file: ' + err);
         } else {
-          console.log('\nService is succesfully installed.\n\n');
+          console.log('\nService is succesfully installed.\n');
           shell.exec('systemctl daemon-reload');
         }
       });
     } else {
-      console.log("\nPlatform is not supported!\n\n");
+      console.log("\nPlatform is not supported!\n");
     }
   },
   remove: function (configOpts, configFileName) {
     if (process.platform == "win32") {
-      procTools.execCommand('cgservice.exe uninstall', function (returnvalue) {
-        console.log("\n" + returnvalue);
-      });
+      shell.exec('cgservice.exe uninstall');
     } else if (process.platform == "linux") {
       fs.unlink("/etc/systemd/system/ccx-guardian.service", function (err) {
         if (err) {
           console.log('\nError trying to remove the service: ' + err);
         } else {
-          console.log('\nService is succesfully removed.\n\n');
+          console.log('\nService is succesfully removed.\n');
         }
       });
     } else {
-      console.log("\nPlatform is not supported!\n\n");
+      console.log("\nPlatform is not supported!\n");
     }
   },
   start: function (configOpts, configFileName) {
     if (process.platform == "win32") {
-      procTools.execCommand('cgservice.exe start', function (returnvalue) {
-        console.log("\n" + returnvalue);
-      });
+      shell.exec('cgservice.exe start');
     } else if (process.platform == "linux") {
       shell.exec('systemctl start ccx-guardian');
       shell.exec('systemctl status ccx-guardian');
     } else {
-      console.log("\nPlatform is not supported!\n\n");
+      console.log("\nPlatform is not supported!\n");
     }
   },
   stop: function (configOpts, configFileName) {
     if (process.platform == "win32") {
-      procTools.execCommand('cgservice.exe stop', function (returnvalue) {
-        console.log("\n" + returnvalue);
-      });
+      shell.exec('cgservice.exe stop');
     } else if (process.platform == "linux") {
       shell.exec('systemctl stop ccx-guardian');
     } else {
-      console.log("\nPlatform is not supported!\n\n");
+      console.log("\nPlatform is not supported!\n");
     }
   },
   status: function (configOpts, configFileName) {
     if (process.platform == "linux") {
       shell.exec('systemctl status ccx-guardian');
     } else {
-      console.log("\nPlatform is not supported!\n\n");
+      console.log("\nPlatform is not supported!\n");
     }
   }
 };
