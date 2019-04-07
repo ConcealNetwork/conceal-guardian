307ed4f4983c83647e5a9a5bc1a157db28d2492e
Taegus <taegus.cromis@gmail.com>
2019-04-07 09:38:12 +0200

    added 7zip to tools build script now also includes all needed files and creates archives

--- build.bat
+++ build.bat
@@ -1,7 +1,18 @@
+@ECHO OFF
+REM Windows build
+del .\bin\win\*.* /F /Q
 call pkg index.js --targets node10-win-x64 --output .\bin\win\guardian-win64.exe
-call pkg index.js --targets node10-linux-x64 --output .\bin\linux\guardian-linux64
-call pkg index.js --targets node10-macos-x64 --output .\bin\macos\guardian-macos64
+copy .\tools\cgservice.exe .\bin\win
+copy config.json.sample .\bin\win\config.json
+.\tools\7z.exe a .\bin\win\guardian-win64.zip .\bin\win\guardian-win64.exe .\bin\win\config.json .\bin\win\cgservice.exe
 
-rem call .\tools\upx.exe .\bin\win\guardian.exe
-rem call .\tools\upx.exe .\bin\linux\guardian
-rem call .\tools\upx.exe .\bin\macos\guardian
+REM Linux build
+del .\bin\linux\*.* /F /Q
+call pkg index.js --targets node10-linux-x64 --output .\bin\linux\guardian-linux64
+copy ccx-guardian.service.template .\bin\linux
+copy config.json.sample .\bin\linux\config.json
+.\tools\7z.exe a -ttar .\bin\linux\guardian-linux64.tar .\bin\linux\guardian-linux64 .\bin\linux\config.json .\bin\linux\ccx-guardian.service.template
+.\tools\7z.exe a -tgzip .\bin\linux\guardian-linux64.tar.gz .\bin\linux\guardian-linux64.tar
+
+REM OSX build (uncomplete, just the binary)
+REM call pkg index.js --targets node10-macos-x64 --output .\bin\macos\guardian-macos64
