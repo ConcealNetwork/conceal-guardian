5384bb8bcaf4e1800616a829183d6e0723243688
Taegus <taegus.cromis@gmail.com>
2019-04-16 16:28:16 +0200

    checking for supported OS version for node executable download

--- units/download.js
+++ units/download.js
@@ -8,37 +8,17 @@ const extractTAR = require('tar');
 const tempDir = require('temp-dir');
 const utils = require("./utils.js");
 const shell = require("shelljs");
+const getos = require('getos');
 const path = require("path");
 const fs = require("fs");
+
+// a message if you are on the wrong OS and there is no precompiled binaries for that OS.
+const wrongLinuxOSMsg = "Only ubuntu 16.04 and 18.04 have precompiled binaries, on other linux systems you need to build the daemon yourself. Reffer to: https://github.com/ConcealNetwork/conceal-core";
+const wrongOSMsg = "This operating system has no precompiled binaries you need to build the daemon yourself. Reffer to: https://github.com/ConcealNetwork/conceal-core";
 
 // Define a function to filter releases.
 function filterRelease(release) {
   return release.prerelease === false;
-}
-
-// Define a function to filter assets.
-function filterAssetNode(asset) {
-  if (process.platform === "win32") {
-    return asset.name.indexOf('win64') >= 0;
-  } else if (process.platform === "linux") {
-    return asset.name.indexOf('ubuntu') >= 0;
-  } else if (process.platform === "darwin") {
-    return asset.name.indexOf('macOS') >= 0;
-  } else {
-    return false;
-  }
-}
-
-function filterAssetGuardian(asset) {
-  if (process.platform === "win32") {
-    return asset.name.indexOf('win64') >= 0;
-  } else if (process.platform === "linux") {
-    return asset.name.indexOf('linux64') >= 0;
-  } else if (process.platform === "darwin") {
-    return asset.name.indexOf('mac64') >= 0;
-  } else {
-    return false;
-  }
 }
 
 function extractArchive(filePath, outDir, callback) {
@@ -73,45 +53,92 @@ module.exports = {
     shell.rm('-rf', finalTempDir);
     shell.mkdir('-p', finalTempDir);
 
-    downloadRelease('ConcealNetwork', 'conceal-core', finalTempDir, filterRelease, filterAssetNode, true)
-      .then(function () {
-        fs.readdir(finalTempDir, function (err, items) {
-          if (items.length > 0) {
-            extractArchive(path.join(finalTempDir, items[0]), finalTempDir, function (success) {
-              if (success) {
-                shell.rm('-rf', path.join(finalTempDir, items[0]));
+    getos(function (error, system) {
+      if (error) {
+        callback(error);
+      } else {
+        if (system.os == "linux") {
+          if (system.dist == "Ubuntu Linux") {
+            if ((system.release !== "16.04") && (system.release !== "18.04")) {
+              callback(wrongLinuxOSMsg);
+              return false;
+            }
+          } else {
+            callback(wrongLinuxOSMsg);
+            return false;
+          }
+        } else if (system.os == "darwin") {
+          callback(wrongOSMsg);
+          return false;
+        }
 
-                fs.readdir(finalTempDir, function (err, items) {
-                  if (items.length > 0) {
-                    if (process.platform === "win32") {
-                      shell.cp(path.join(finalTempDir, utils.getNodeExecutableName()), path.dirname(nodePath));
-                    } else {
-                      shell.cp(path.join(finalTempDir, items[0], utils.getNodeExecutableName()), path.dirname(nodePath));
-                    }
-                    shell.rm('-rf', finalTempDir);
-                    shell.chmod('+x', nodePath);
-                    callback(null);
+        // Define a function to filter assets.
+        var filterAssetNode = function (asset) {
+          if (process.platform === "win32") {
+            return asset.name.indexOf('win64') >= 0;
+          } else if (process.platform === "linux") {
+            return asset.name.indexOf('ubuntu') >= 0;
+          } else if (process.platform === "darwin") {
+            return asset.name.indexOf('macOS') >= 0;
+          } else {
+            return false;
+          }
+        };
+
+        downloadRelease('ConcealNetwork', 'conceal-core', finalTempDir, filterRelease, filterAssetNode, true)
+          .then(function () {
+            fs.readdir(finalTempDir, function (err, items) {
+              if (items.length > 0) {
+                extractArchive(path.join(finalTempDir, items[0]), finalTempDir, function (success) {
+                  if (success) {
+                    shell.rm('-rf', path.join(finalTempDir, items[0]));
+
+                    fs.readdir(finalTempDir, function (err, items) {
+                      if (items.length > 0) {
+                        if (process.platform === "win32") {
+                          shell.cp(path.join(finalTempDir, utils.getNodeExecutableName()), path.dirname(nodePath));
+                        } else {
+                          shell.cp(path.join(finalTempDir, items[0], utils.getNodeExecutableName()), path.dirname(nodePath));
+                        }
+                        shell.rm('-rf', finalTempDir);
+                        shell.chmod('+x', nodePath);
+                        callback(null);
+                      } else {
+                        callback("No downloaded archives found");
+                      }
+                    });
                   } else {
-                    callback("No downloaded archives found");
+                    callback("Failed to extract the archive");
                   }
                 });
               } else {
-                callback("Failed to extract the archive");
+                callback("No downloaded archives found");
               }
             });
-          } else {
-            callback("No downloaded archives found");
-          }
-        });
-      })
-      .catch(function (err) {
-        callback(err.message);
-      });
+          })
+          .catch(function (err) {
+            callback(err.message);
+          });
+      }
+    });
   },
   downloadLatestGuardian: function (nodePath, callback) {
     var finalTempDir = path.join(tempDir, utils.ensureNodeUniqueId());
     shell.rm('-rf', finalTempDir);
     shell.mkdir('-p', finalTempDir);
+
+    // Define a function to filter assets.
+    var filterAssetGuardian = function (asset) {
+      if (process.platform === "win32") {
+        return asset.name.indexOf('win64') >= 0;
+      } else if (process.platform === "linux") {
+        return asset.name.indexOf('linux64') >= 0;
+      } else if (process.platform === "darwin") {
+        return asset.name.indexOf('mac64') >= 0;
+      } else {
+        return false;
+      }
+    };
 
     downloadRelease('ConcealNetwork', 'conceal-guardian', finalTempDir, filterRelease, filterAssetGuardian, true)
       .then(function () {
