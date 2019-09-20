82a13ca4ea6b0560c7f770370a48ad4e5a9dc7a5
Taegus <taegus.cromis@gmail.com>
2019-09-20 19:45:33 +0200

    added peer map navigation selector highlighting

--- units/apiServer.js
+++ units/apiServer.js
@@ -3,6 +3,7 @@
 // Please see the included LICENSE file for more information.
 const readLastLines = require('read-last-lines');
 const express = require("express");
+const geoip = require('geoip-lite');
 const utils = require("./utils.js");
 const path = require("path");
 
@@ -33,6 +34,19 @@ module.exports = {
       });
     });
 
+    app.get("/getPeersData", (req, res) => {
+      var statusResponse = onDataCallback();
+      var peerGeoData = [];
+
+      if ((statusResponse.blockchain) && (statusResponse.blockchain.connections)) {
+        statusResponse.blockchain.connections.forEach(function (value) {
+          peerGeoData.push(geoip.lookup(value));
+        });
+      }
+
+      res.send(peerGeoData);
+    });
+
     app.get("/index.html", (req, res) => {
       res.sendFile(path.resolve('html/index.html'));
     });
@@ -45,8 +59,16 @@ module.exports = {
       res.sendFile(path.resolve('html/daemonLog.html'));
     });
 
+    app.get("/peers.html", (req, res) => {
+      res.sendFile(path.resolve('html/peers.html'));
+    });
+
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve('html' + req.path));
+      if (path.extname(req.path) !== '.map') {
+        res.sendFile(path.resolve('html' + req.path));
+      } else {
+        res.status(404).send('Not found');
+      }
     });
   }
 };
