973af2dd140ba06d886788afc4bf2fd65cc85959
Taegus <taegus.cromis@gmail.com>
2024-02-27 11:51:23 +0100

    linux build script updated

--- build.bat
+++ build.bat
@@ -8,10 +8,9 @@ call nexe index.js --build -o .\bin\win\guardian-win64.exe
 copy .\tools\cgservice.exe .\bin\win
 xcopy /s /q /i html bin\win\html
 xcopy /s /q /i dist\index.js bin\win\index.js*
-xcopy /s /q /i dist\config.json bin\win\config.json*
-xcopy /s /q /i dist\package.json bin\win\package.json*
 xcopy /s /q /i dist\exec-child.js bin\win\exec-child.js*
 copy .\commands\win\*.* .\bin\win
 copy exclude.txt .\bin\win\exclude.txt
+copy package.json .\bin\win\package.json
 copy config.json.sample .\bin\win\config.json
 .\tools\7z.exe a -r .\bin\win\guardian-win64.zip .\bin\win\*.*
--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ cp ./config.json.sample ./bin/linux/config.json
 cp ./ccx-guardian.service.template ./bin/linux
 cp ./dist/index.js ./bin/linux/index.js
 cp ./dist/exec-child.js ./bin/linux/exec-child.js
-tar -czf ./bin/linux/guardian-linux64.tar.gz -C ./bin/linux/guardian-linux64 ./bin/linux/index.js ./bin/linux/exec-child.js ./bin/linux/config.json ./bin/linux/package.json exclude.txt ccx-guardian.service.template html
+tar -czf ./bin/linux/guardian-linux64.tar.gz --exclude guardian-linux64.tar.gz -C ./bin/linux .
