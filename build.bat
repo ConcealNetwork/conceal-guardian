6a4d5430ab297d01b464766af25fdfe99fef77e7
Taegus <taegus.cromis@gmail.com>
2024-02-27 11:20:50 +0100

    updated the build script added package script to package.json

--- build.bat
+++ build.bat
@@ -1,25 +1,17 @@
 @ECHO OFF
 REM Windows build
 del .\bin\win\*.* /F /Q
+del .\dist\*.* /F /Q
 for /D %%p in (".\bin\win\*.*") do rmdir "%%p" /s /q
-call pkg index.js --targets node18-win-x64 --output .\bin\win\guardian-win64.exe
+call npm run package
+call nexe index.js --build -o .\bin\win\guardian-win64.exe
 copy .\tools\cgservice.exe .\bin\win
 xcopy /s /q /i html bin\win\html
+xcopy /s /q /i dist\index.js bin\win\index.js*
+xcopy /s /q /i dist\config.json bin\win\config.json*
+xcopy /s /q /i dist\package.json bin\win\package.json*
+xcopy /s /q /i dist\exec-child.js bin\win\exec-child.js*
 copy .\commands\win\*.* .\bin\win
 copy exclude.txt .\bin\win\exclude.txt
 copy config.json.sample .\bin\win\config.json
 .\tools\7z.exe a -r .\bin\win\guardian-win64.zip .\bin\win\*.*
-
-REM Linux build
-del .\bin\linux\*.* /F /Q
-for /D %%p in (".\bin\linux\*.*") do rmdir "%%p" /s /q
-call pkg index.js --targets node18-linux-x64 --output .\bin\linux\guardian-linux64
-xcopy /s /q /i html bin\linux\html
-copy exclude.txt .\bin\linux\exclude.txt
-copy ccx-guardian.service.template .\bin\linux
-copy config.json.sample .\bin\linux\config.json
-.\tools\7z.exe a -r -ttar .\bin\linux\guardian-linux64.tar .\bin\linux\guardian-linux64 .\bin\linux\config.json .\bin\linux\exclude.txt .\bin\linux\ccx-guardian.service.template .\bin\linux\html
-.\tools\7z.exe a -tgzip .\bin\linux\guardian-linux64.tar.gz .\bin\linux\guardian-linux64.tar
-
-REM OSX build (uncomplete, just the binary)
-REM call pkg index.js --targets node10-macos-x64 --output .\bin\macos\guardian-macos64\
