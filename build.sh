c32b427923d85a7a8e93dd01f5d473e0aa12771e
Taegus <taegus.cromis@gmail.com>
2024-02-27 11:28:32 +0100

    linux build updated

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+rm -rf ./dist/*
 rm -rf ./bin/linux/*
 mkdir -p ./bin/linux
-pkg index.js --targets node10-linux-x64 --output ./bin/linux/guardian-linux64
+npm run package
+nexe index.js --build -o ./bin/linux/guardian-linux64
 cp -R ./html ./bin/linux/html
 cp ./exclude.txt ./bin/linux/exclude.txt
-cp ./ccx-guardian.service.template ./bin/linux
+cp ./package.json ./bin/linux/package.json
 cp ./config.json.sample ./bin/linux/config.json
-tar -czf ./bin/linux/guardian-linux64.tar.gz -C ./bin/linux guardian-linux64 config.json exclude.txt ccx-guardian.service.template html
+cp ./ccx-guardian.service.template ./bin/linux
+cp ./dist/index.js ./bin/linux/index.js
+cp ./dist/exec-child.js ./bin/linux/exec-child.js
+tar -czf ./bin/linux/guardian-linux64.tar.gz -C ./bin/linux/guardian-linux64 ./bin/linux/index.js ./bin/linux/exec-child.js ./bin/linux/config.json ./bin/linux/package.json exclude.txt ccx-guardian.service.template html
