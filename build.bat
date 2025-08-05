762ca4167458c3b4467ced1a5ac2d33eeebdc345
Acktarius <acktarius@protonmail.com>
2025-08-05 08:26:13 -0300

    wip ubuntu builds

--- build.bat
+++ build.bat
@@ -3,11 +3,13 @@ REM Windows build
 del .\bin\win\*.* /F /Q
 del .\dist\*.* /F /Q
 for /D %%p in (".\bin\win\*.*") do rmdir "%%p" /s /q
+if not exist ".\dist" mkdir ".\dist"
+if not exist ".\bin\win" mkdir ".\bin\win"
 call npm run package
-call nexe index.js --build --target=22.18.0 -o .\bin\win\guardian-win64.exe
+xcopy /s /q /i dist .\bin\win
+call npx nexe --input .\bin\win\index.js --build --target=22.18.0 --bundle -o .\bin\win\guardian-win64.exe
 copy .\tools\cgservice.exe .\bin\win
 xcopy /s /q /i html bin\win\html
-xcopy /s /q /i dist\index.js bin\win\index.js*
 copy .\commands\win\*.* .\bin\win
 copy exclude.txt .\bin\win\exclude.txt
 copy package.json .\bin\win\package.json
